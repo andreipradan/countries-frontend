7dd5b57aec9bacf39b5824ed177664975ca6c2e9
Andrei Pradan <andrei.pradan@gmail.com>
2022-08-31 17:27:18 +0300

    replaced secret key with env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dou7i7-l75wha*_585%y@)(%7yad2gy1oaj-b$g3rl37o1x!f6'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
