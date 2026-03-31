63425e78cf42cece73075f13f8be4106c4700569
Andrei Prădan <andrei.pradan@gmail.com>
2026-03-31 22:16:01 +0300

    Update API base URL to use environment variable

--- src/api.js
+++ src/api.js
@@ -3,5 +3,5 @@ import axios from 'axios'
 export default axios.create({
 	baseURL: process.env.NODE_ENV === 'development'
 		? 'http://127.0.0.1:8000/'
-		: 'https://api.countries.pradan.dev/'
+		: `${process.env.REACT_APP_API_URL}/`
 })
